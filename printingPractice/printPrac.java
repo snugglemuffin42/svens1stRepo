029da4255269aa3239849eb72234c88bad18674e
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-29 14:22:23 +0000

    this is my first print code with a bunch printed

--- printingPractice/printPrac.java
+++ printingPractice/printPrac.java
@@ -2,13 +2,16 @@ class printPrac
 {
     public static void main(String args[])
     {
-
+            System.out.println("");
         System.out.println("Welcome User! ");
-
+            System.out.println("");
         System.out.print("User's Name: Sven Ashbrook, ");
-
+            System.out.println("");
+            System.out.println("");
         System.out.print("User's Age: 14, ");
-
+            System.out.println("");
+            System.out.println("");
         System.out.println("Thanks for visiting my Greet'n Print!");
+            System.out.println("");
     }
 }
