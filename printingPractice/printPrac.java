2ea5477dff844aec06370ed3a3e9840320536f6e
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-29 14:11:48 +0000

    this is my 1st printing practice with more items

---
+++ printingPractice/printPrac.java
@@ -0,0 +1,14 @@
+class printPrac
+{
+    public static void main(String args[])
+    {
+
+        System.out.println("Welcome User! ");
+
+        System.out.print("User's Name: Sven Ashbrook, ");
+
+        System.out.print("User's Age: 14, ");
+
+        System.out.println("Thanks for visiting my Greet'n Print!");
+    }
+}
