89a951f4f33e86bff234c4b9995435a05570f4a0
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-31 13:26:22 +0000

    This is my 1st/5 new assignments.

---
+++ morePractice/NumberPatternPrinter.java
@@ -0,0 +1,19 @@
+/*Sven Ashbrook*/
+class NumberPatternPrinter
+{
+    public static void main(String args[])
+    {
+            System.out.println("");
+        System.out.println("Hello and Welcome! ");
+            System.out.println("");
+        System.out.print("1");
+            System.out.println("");
+            System.out.println("2 3");
+            System.out.println("4 5 6");
+        System.out.print("7 8 9 10");
+            System.out.println("");
+            System.out.println("");
+        System.out.println("That's all, Thanks!");
+            System.out.println("");
+    }
+}
