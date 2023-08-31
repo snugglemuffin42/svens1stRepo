01d332eefc3c6ae734c4156752a5aad3392c3f5f
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-31 13:38:09 +0000

    This is my 2nd/5 assignments. This is a $quare.

---
+++ morePractice/PrintArt.java
@@ -0,0 +1,25 @@
+/*Sven Ashbrook*/
+class PrintArt
+{
+    public static void main(String args[])
+    {
+            System.out.println("");
+        System.out.println("Hello! Enjoy my fun shape! ");
+            System.out.println("");
+        System.out.println("$ $ $ $ $ $ $ $ $ $ $ $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+            System.out.println("$                     $");
+        System.out.println("$ $ $ $ $ $ $ $ $ $ $ $");
+            System.out.println("");
+            System.out.println("");
+        System.out.println("Thank you for visiting my Art Printer!");
+
+    }
+}
