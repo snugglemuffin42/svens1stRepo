da32a9a55f2d66686c0b3f31f7d879ba7ed49b72
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-31 13:50:21 +0000

    This is my second star art. Enjoy!

---
+++ morePractice/StarArt2.java
@@ -0,0 +1,17 @@
+/*Sven Ashbrook*/
+class NumberPatternPrinter
+{
+    public static void main(String args[])
+    {
+            System.out.println("");
+        System.out.println("Enjoy my second Star Art! ");
+            System.out.println("");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * * *");
+            System.out.println("");
+        System.out.println("Thank you for viewing my second art!");
+    }
+}
