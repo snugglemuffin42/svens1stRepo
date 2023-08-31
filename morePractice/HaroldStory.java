5baadc330a537e0fb5e24479e979e79bd56de8fd
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-08-31 14:16:16 +0000

    This is my last assignment about a dog, Harold.

---
+++ morePractice/HaroldStory.java
@@ -0,0 +1,24 @@
+/*Sven Ashbrook*/
+class HaroldStory
+{
+    public static void main(String args[])
+    {
+            System.out.println("");
+        System.out.println("Harold and the Cat");
+            System.out.println("");
+        System.out.println("Harold the dog was never the sharpest tool in the shed, but he sure had a" 
+            + "big heart. One day, he stumbled upon a cat with a top hat. ");
+        System.out.println("Harold: Hey there, what's up with the fancy hat?");
+        System.out.println("The cat scoffed,");
+        System.out.print("Cat with Top Hat: It's called fashion, darling.");
+        System.out.println(" Harold tilted his head,");
+        System.out.println("Harold: What's that?");
+        System.out.println("The cat rolled her eyes,");
+        System.out.print("Cat: Never mind, let's just focus on finding you a bone.");
+        System.out.println(" Harold wagged his tail in excitement,");
+        System.out.println("Harold: Yippe!");
+        System.out.println("And they trotted off together.");
+            System.out.println("");
+        System.out.println("Thank You for reading!");
+    }
+}
