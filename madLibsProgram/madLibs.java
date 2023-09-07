a2025ea37b717f587ed88f49da1e9eb7711c9564
snugglemuffin42 <142909661+snugglemuffin42@users.noreply.github.com>
2023-09-07 13:56:13 +0000

    This is a mad lib I made about a pirate dog.

---
+++ madLibsProgram/madLibs.java
@@ -0,0 +1,26 @@
+class madLibs
+{
+    public static void main(String args [])
+    {
+        String adjective1 = "frightening";
+        String adjective2 = "tough";
+        String noun1 = "ship";
+        String pluralNoun1 = "strays";
+        String noun2 = "seas";
+        String noun3 = "peanut butter";
+        String pluralNoun2 = "crew";
+        String noun4 = "Bone Buccaneer";
+        String noun5 = "cat";
+        String exclamation1 = "howl";
+
+        System.out.println("");
+        
+        System.out.println("Once upon a time in a world of " + adjective1 + " pirates, there lived a dog named Captain Barkbeard. ");
+        System.out.println("This " + adjective2 + " pup had a mighty " + noun1 + " and a crew of " + pluralNoun1 + " who were as loyal as can be. ");
+        System.out.println("Captain Barkbeard sailed the seven " + noun2 + " in search of the legendary treasure filled with " + noun3);
+        System.out.println("One day, as Captain Barkbeard and his " + pluralNoun2 + " set sail on their trusty ship, the " + noun4 + ", they encountered a fearsome " + noun5 + " that blocked their path. ");
+        System.out.println("With a hearty " + exclamation1 + ", Captain Barkbeard barked orders, and the crew sprang into action, ready to face any challenge that came their way. ");
+        System.out.println("");
+        System.out.println("");
+    }
+}
